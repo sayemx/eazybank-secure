98d15a3bd136357f7233c0fc30d88d4088aed251
sayemx <sayem.saqueeb@splashbi.com>
2025-08-06 22:50:26 +0530

    Initital commit with section 1 complete - Getting Started with default spring security.

---
+++ src/main/java/com/sayem/eazybank/EazybankSecureApplication.java
@@ -0,0 +1,13 @@
+package com.sayem.eazybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazybankSecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazybankSecureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sayem/eazybank/EazybankSecureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayem.eazybank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazybankSecureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
