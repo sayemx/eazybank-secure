45f782356f0666ac0d980e338e2aa2eea8cee8d8
sayemx <t_sayeem.saqueeb@splashbi.com>
2025-08-07 02:03:13 +0530

    Section 3 - Defining & Managing users using InMemoryUserDetailsmanager

--- src/main/java/com/sayem/eazybank/ProjectSecurityConfig.java
+++ src/main/java/com/sayem/eazybank/ProjectSecurityConfig.java
@@ -4,8 +4,16 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -22,6 +30,26 @@ public class ProjectSecurityConfig {
 		http.formLogin(withDefaults());
 		http.httpBasic(withDefaults());
 		return http.build();
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails sayem1 = User.withUsername("sayem1").password("{noop}sayem@123321").authorities("read").build();
+		UserDetails admin1 = User.withUsername("admin1").password("{noop}admin123").authorities("write").build();
+		
+		UserDetails sayem2 = User.withUsername("sayem2").password("{bcrypt}$2a$12$wMABk4zbNj3bjfYq8nz5y.zE/.k2bJtJJUBf7k7osZ9Jx.KTbrg5O").authorities("read").build();
+		UserDetails admin2 = User.withUsername("admin2").password("{bcrypt}$2a$12$bEJZD4oHxBheF4vM4aT7aeVjZLhy9VOKcAiJfDbB0DXUKcdjdKD5S").authorities("write").build();
+		return new InMemoryUserDetailsManager(sayem1, admin1, sayem2, admin2);
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+	
+	@Bean
+	public CompromisedPasswordChecker compromisedPasswordChecker() {
+		return new HaveIBeenPwnedRestApiPasswordChecker();
 	}
 	
 }
